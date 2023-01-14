2792871b3400efcc74d43e7416ddb05f2f104efb
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-01-14 23:51:46 +0000

    return random film

--- assets/index.js
+++ assets/index.js
@@ -10,7 +10,7 @@ const certURL = "certification/movie/list?api_key=";
 // API Key
 const APIKEY = "c4321cfbc4e58956270feef6a91120a8";
 
-// API Queries 
+// API Queries - **SOME ARE HARDCODED WITH VALUES AT THE MOMENT**
 let watchRegion = "&watch_region=GB";
 // This searches for Netflix (8) or Disney Plus (337)
 let watchProviderTest = "&with_watch_providers=337|8";
@@ -19,31 +19,33 @@ let minVotes = "&vote_count.gte=3500"
 let lang = "&language=en-US";
 let certificationCountry = "&certification_country=GB";
 let familyCerts = "&certification=PG%7CU";
-let movieID = "1726";
 let sortByPop = "&sort_by=popularity.desc";
 let removeAdult = "&include_adult=false";
 let removeTrailers = "&include_video=false";
-let page = "&page=1&page=2";
+let page = "&page=1";
 let freeWithSub = "&with_watch_monetization_types=flatrate";
 let longFilms = "&with_runtime.gte=150"
 let shortFilms = "&with_runtime.lte=100"
 
+// Generate pseudo-random number
+function getRandom() {
+    return Math.floor(Math.random() * 19);
+    }
 
 // Object Maps
-
-// Certs     *****We may not need this, can hardcode U & PG***
-function certMap() {
-    let queryURL = baseURL + certURL + APIKEY 
-    console.log("cert URL" + queryURL)
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    })
-    .then(function(response) {
-        console.log("certs: ")
-        console.log(response)
-    })
-}
+// Certs     *** We may not need this, can just hardcode U & PG ***
+// function certMap() {
+//     let queryURL = baseURL + certURL + APIKEY 
+//     console.log("cert URL" + queryURL)
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET",
+//     })
+//     .then(function(response) {
+//         console.log("certs: ")
+//         console.log(response)
+//     })
+// }
 
 // Create Genre Object Map 
 const genres = new Map();
@@ -82,8 +84,9 @@ function providerMap() {
     })
 }
 
-// Get array of films
+// GET FILM ARRAY AND RANDOMLY SELECT ONE
 function filmSearch() {
+    // Build URL
     let queryURL = baseURL + discoverURL + APIKEY + lang + sortByPop + minVotes + certificationCountry + removeAdult + removeTrailers + page + genre + watchProviderTest + watchRegion + freeWithSub + longFilms;
     console.log("filmSearch URL: " + queryURL)
     $.ajax({
@@ -92,15 +95,14 @@ function filmSearch() {
     })
     .then(function(response) {
         console.log("film search: " + JSON.stringify(response))
-        // ADD RANDOM FILM SELECTOR
-        // STORE FILM ID AND PASS TO getFILM
-        console.log("film id: " + (response.results[0].id))
-        getFilm()
+        console.log("film id: " + (response.results[getRandom()].id))
+        let movieID = response.results[getRandom()].id
+        getFilm(movieID)
     })
 }
 
 // Get info for a given film
-function getFilm() {
+function getFilm(movieID) {
     let queryURL = baseURL + movieInfoURL + movieID + "?api_key=" + APIKEY + lang
     console.log("getFilm URL: " + queryURL)
     $.ajax({
@@ -114,5 +116,4 @@ function getFilm() {
 }
     genreMap();
     providerMap();
-    certMap();
     filmSearch();
