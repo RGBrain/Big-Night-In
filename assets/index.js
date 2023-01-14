6767ffb2495cc891bab0550df50c5f613e88ffb0
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-01-14 12:01:23 +0000

    add watch provider object map

--- assets/index.js
+++ assets/index.js
@@ -1,52 +1,82 @@
 // API settings 
 
 // TMDB (Movie Database)
+const baseURL = "https://api.themoviedb.org/3/";
+const discoverURL = "https://api.themoviedb.org/3/discover/movie?api_key=";
+const movieInfoURL = "https://api.themoviedb.org/3/movie/";
+const genreURL = "genre/movie/list?api_key="
+const providerURL = "watch/providers/movie?api_key="
+// API Key
+const APIKEY = "c4321cfbc4e58956270feef6a91120a8";
+
+// https://api.themoviedb.org/3/watch/providers/regions?api_key=c4321cfbc4e58956270feef6a91120a8&language=en-US
+
+// https://api.themoviedb.org/3/watch/providers/movie?api_key=c4321cfbc4e58956270feef6a91120a8&language=en-US&watch_region=GB
 
 // genre
 // Family friendly?
 // length?  Short - Epic - I don't care
 
-
-
 // ? PLACEHOLDER VARIABLES FOR NOW, BUT WILL BE TAKEN DYNAMICALLY FROM USER.
-let watchRegion = "&^GB$";
-let watchProviders = "&with_watch_providers=337";
+// let watchRegion = "&^GB$";
+let watchRegion = "&watch_region=GB"
+let watchProviderTest = "&with_watch_providers=337";
 let genre = "&with_genres=35";
 let rating = "";
 let lang = "&language=en-US";
 let cert = "";
 let certificationCountry = "GB"
 let movieID = "899112"
-const baseDiscoverURL = "https://api.themoviedb.org/3/discover/movie?api_key=";
-const movieInfoURL = "https://api.themoviedb.org/3/movie/";
-const genreURL = "https://api.themoviedb.org/3/genre/movie/list?api_key="
-// make empty object map, do api call, loop results and add to map
-const APIKEY = "c4321cfbc4e58956270feef6a91120a8";
+// Object Maps
 
-// Get Genre List
-function genreSearch() {
-    let queryURL = genreURL + APIKEY + genre + lang
-    console.log("filmSearch URL: " + queryURL)
+// Create Genre Object Map 
+const genres = new Map();
+function genreMap() {
+    let queryURL = baseURL + genreURL + APIKEY 
+    console.log(queryURL)
     $.ajax({
         url: queryURL,
         method: "GET",
     })
     .then(function(response) {
-        console.log("genre search:- ")
-        console.log(response)
+        // Build object map
+        for (let i = 0; i < response.genres.length; i++) {
+            genres.set(response.genres[i].name, response.genres[i].id);
+        }
+        console.log(genres)
+        console.log(" The code is: " + genres.get("Documentary"))
+    })
+}
+
+// Create Watch Provider Object Map 
+const watchProviders = new Map();
+function providerMap() {
+    let queryURL = baseURL + providerURL + APIKEY + lang + watchRegion;
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    })
+    .then(function(response) {
+        console.log("line 60: " + response.results.provider_name)
+        // Build object map
+        for (let i = 0; i < response.results.length; i++) {
+            watchProviders.set(response.results[i].provider_name, response.results[i].provider_id);
+        }
+        console.log(watchProviders)
     })
 }
 
 // Get array of films
 function filmSearch() {
-    let queryURL = baseDiscoverURL + APIKEY + genre
+    let queryURL = discoverURL + APIKEY + genre
     console.log("filmSearch URL: " + queryURL)
     $.ajax({
         url: queryURL,
         method: "GET",
     })
     .then(function(response) {
-        console.log("film search: " + JSON.stringify(response))
+        // console.log("film search: " + JSON.stringify(response))
         getFilm()
     })
 }
@@ -54,15 +84,16 @@ function filmSearch() {
 // Get film information
 function getFilm() {
     let queryURL = movieInfoURL + movieID + "?api_key=" + APIKEY
-    console.log("getFilm URL: " + queryURL)
+    // console.log("getFilm URL: " + queryURL)
     $.ajax({
         url: queryURL,
         method: "GET",
     })
     .then(function(response) {
         console.log
-        console.log("film info: " + JSON.stringify(response))
+        // console.log("film info: " + JSON.stringify(response))
     })
 }
-    genreSearch();
+    genreMap();
+    providerMap()
     filmSearch();
