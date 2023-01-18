fc84d8e3f6022b74007bfab80f9cfe3ac37b484f
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-01-18 10:34:45 +0000

    Merge API

--- assets/index.js
+++ assets/index.js
@@ -14,7 +14,9 @@ const APIKEY = "c4321cfbc4e58956270feef6a91120a8";
 let watchRegion = "&watch_region=GB";
 // This searches for Netflix (8) or Disney Plus (337)
 let watchProviderTest = "&with_watch_providers=337|8";
-let genre = "&with_genres=28";
+let genre = "&with_genres=28"
+// let genreChoice = THE USER INPUT HERE
+// let genreID = genres.get(genreChoice)
 let minVotes = "&vote_count.gte=3500"
 let lang = "&language=en-US";
 let certificationCountry = "&certification_country=GB";
@@ -24,8 +26,8 @@ let removeAdult = "&include_adult=false";
 let removeTrailers = "&include_video=false";
 let page = "&page=1";
 let freeWithSub = "&with_watch_monetization_types=flatrate";
-let longFilms = "&with_runtime.gte=150"
-let shortFilms = "&with_runtime.lte=100"
+let longFilms = "&with_runtime.gte=150";
+let shortFilms = "&with_runtime.lte=100";
 
 // Generate pseudo-random number
 function getRandom() {
@@ -62,7 +64,7 @@ function genreMap() {
             genres.set(response.genres[i].name, response.genres[i].id);
         }
         console.log(genres)
-        console.log(" The code is: " + genres.get("Documentary"))
+        console.log(" The code is: " + genres.get("Comedy"))
     })
 }
 
@@ -117,3 +119,43 @@ function getFilm(movieID) {
     genreMap();
     providerMap();
     filmSearch();
+
+// 
+// TOMTOM API
+// 
+
+// ? Request Format: https://{baseURL}/search/{versionNumber}/search/{query}.{ext}?key={Your_API_Key}&typeahead={typeahead}&limit={limit}&ofs={ofs}&countrySet={countrySet}&lat={lat}&lon={lon}&radius={radius}&topLeft={topLeft}&btmRight={btmRight}&language={language}&idxSet={idxSet}&extendedPostalCodesFor={extendedPostalCodesFor}&minFuzzyLevel={minFuzzyLevel}&maxFuzzyLevel={maxFuzzyLevel}&categorySet={categorySet}&brandSet={brandSet}&connectorSet={connectorSet}&fuelSet={fuelSet}&view={view}&openingHours={openingHours}&timeZone={timeZone}&mapcodes={mapcodes}&relatedPois={relatedPois}&minPowerKW={minPowerKW}&maxPowerKW={maxPowerKW}&entityTypeSet={entityTypeSet}
+
+// ? Request Example: https://api.tomtom.com/search/2/search/36.98844,-121.97483.json?key={Your_API_Key}
+
+// ? Geocode Example: https://api.tomtom.com/search/2/geocode/De Ruijterkade 154, 1011 AC, Amsterdam.json?key={Your_API_Key}
+
+// ! https://api.tomtom.com/search/2/categorySearch/pizza.json?lat=37.337&lon=-121.89&view=Unified&relatedPois=false&key=*****
+
+// ! https://api.tomtom.com/search/2/poiCategories.json?key={Your_API_Key}
+
+
+const ttBaseURL = "https://api.tomtom.com/search/2/";
+const ttAPIKey = "jxWprAPAeXwm1cbD0NLRVPErGVwfEn1u";
+let geo =  "?lat=52.111&lon=-2.32"
+const ttCatSearch = "categorySearch/"
+let foodType = "pizza";
+let format = ".json"
+let URLEnd = "&view=Unified&relatedPois=false&key="
+
+// API Test
+function findFood() {
+    // let queryURL = ttBaseURL + ttCatSearch + foodType + format + geo + URLEnd + ttAPIKey;
+    let queryURL = "https://api.tomtom.com/search/2/search/52.1116,-2.3293.json?key=" + ttAPIKey;
+    // let queryURL = "https://api.tomtom.com/search/2/poiCategories.json?key=" + ttAPIKey;
+    console.log("API Test: " + queryURL)
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    })
+    .then(function(response) {
+        console.log(response)
+        console.log("TOMTOM API: " + JSON.stringify(response))
+    })
+}
+    findFood();
